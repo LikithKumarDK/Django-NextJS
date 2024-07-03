7f3976e8d234b55266e4ee9c23ee7972617f7d77
LikithKumarDK41 <likithkumar.dk@nichi.com>
2024-07-03 23:41:25 +0530

    Update/Check: Sonar cube

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -10,11 +10,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    // <html lang="en">
-    //   <body className={inter.className}>{children}</body>
-    // </html>
-    <>
-      <div className={inter.className}>{children}</div>
-    </>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+    // <>
+    //   <div className={inter.className}>{children}</div>
+    // </>
   );
 }
